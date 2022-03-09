4d83120aaa2e0dcd11d7b83492eee61f01649087
Roman <kruzhkofv@yandex.ru>
2022-03-09 10:50:30 +0300

    step 2

--- math1.py
+++ math1.py
@@ -1,2 +1,15 @@
-import numpy
+import numpy as np
+import matplotlib..pyplot as plt
+
+x = np.linspace(0.0, 10.0, 30) 
+# массив координат - 30 точек, равнораспределенных 
+# в диапозоне от 0 до 30 
+
+y = np.sin(x)
+
+# рисование графики функции с помощью функции plot
+
+plt.plot(x, y)
+
+plt.show()
 
